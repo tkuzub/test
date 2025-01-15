a707de6556ec7a2b7a98d9974a1a3e0bca29cce7
Taras Kuzub <taras.kuzub.ua@gmail.com>
2025-01-15 15:13:55 +0100

    first commit

---
+++ src/test/java/com/tkuzub/springbootstarter/SpringbootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkuzub.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
