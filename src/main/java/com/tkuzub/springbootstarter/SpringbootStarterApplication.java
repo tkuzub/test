af28075bc944014dda8cfdf58c3e534360cb86d8
tkuzub <taras.kuzub.ua@gmail.com>
2025-01-30 15:54:55 +0100

    add commint

--- src/main/java/com/tkuzub/springbootstarter/SpringbootStarterApplication.java
+++ src/main/java/com/tkuzub/springbootstarter/SpringbootStarterApplication.java
@@ -2,12 +2,20 @@ package com.tkuzub.springbootstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootStarterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootStarterApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String greeting() {
+        return "Hello World";
     }
 
 }
